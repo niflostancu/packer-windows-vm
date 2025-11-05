f4879975c4950f6e45ac0207d1ea08e26e2f65c7
Stancu Florin <niflostancu@gmail.com>
2025-11-05 19:22:10 +0200

    cleanup: re-add compact, remove SoftwareDistribution, show disk usage

--- vm/scripts/90-cleanup.ps1
+++ vm/scripts/90-cleanup.ps1
@@ -10,8 +10,13 @@ dism.exe /online /Quiet /Cleanup-Image /StartComponentCleanup /ResetBase
 sc.exe config wuauserv start= disabled
 sc.exe stop wuauserv
 
+# Clean SoftwareDistribution directory with leftover updates
+Remove-Item "$env:SystemRoot\SoftwareDistribution*" -Recurse -Force -ErrorAction SilentlyContinue
+
 # From: https://github.com/windowsbox/powershellmodules (WindowsBox.Compact)
-# Optimize-DiskUsage
+Optimize-DiskUsage
+
+Get-PSDrive -PSProvider FileSystem
 
 exit 0
 
