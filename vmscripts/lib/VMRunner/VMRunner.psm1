9af6290dd42097b57b818c53830e2b9bf4338a48
Stancu Florin <niflostancu@gmail.com>
2026-01-27 12:17:01 +0200

    lib: VMRunner: new VMRebootingTask helper
    Starts a long-running task that may reboot (e.g., BoxRunner script) with
    Packer workarounds (disable WinRM and enable after everything finished).

--- vmscripts/lib/VMRunner/VMRunner.psm1
+++ vmscripts/lib/VMRunner/VMRunner.psm1
@@ -52,7 +52,7 @@ $innerScript
             -Namespace "Root/Microsoft/Windows/TaskScheduler")
         Settings = (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries `
             -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd)
-        TaskName = "${taskPrefix}_$TaskID"
+        TaskName = $schedTaskName
     }
     Write-Verbose "Scheduled powershell.exe with args: '$psArgs'"
 
@@ -101,5 +101,73 @@ $innerScript
     }
 }
 
+function Invoke-VMRebootingTask {
+    param(
+        [parameter(ParameterSetName="runfile")][string]$ScriptFile,
+        [parameter(ParameterSetName="runcommand")][string]$ScriptSnippet, 
+        [string] $ExtraPSArguments = "",
+        [string] $TaskID = "reboot_script",
+        [string] $LogTo = $defaultLogPath
+    )
+    $schedTaskName = $taskPrefix + "rbt_" + "$TaskID"
+    if (Get-ScheduledTask -TaskName $schedTaskName -ErrorAction SilentlyContinue) {
+        Unregister-ScheduledTask -TaskName $schedTaskName -Confirm:$false -ErrorAction SilentlyContinue
+        Start-Sleep -Seconds 1
+    }
+
+    $genScriptPath = "$defaultScriptPath\${taskPrefix}_${TaskID}.ps1"
+    $logFile = "$LogTo\${taskPrefix}_${TaskID}.log"
+
+    $innerScript = $ScriptSnippet
+    if ($ScriptFile) {
+        $innerScript = "& '$ScriptFile'"
+    }
+    $genScript = @"
+& {
+Start-Sleep 8
+Stop-Service winrm | out-null
+Set-Service WinRM -StartupType Disabled | out-null
+Start-Sleep 1
+# abort previous shutdown (will issue one later)
+`$PSNativeCommandUseErrorActionPreference = `$false
+shutdown /a
+
+$innerScript
+} *>&1 | Tee-Object -FilePath '$logFile' -Append
+"@
+    $genScript | Out-File "$genScriptPath" -Encoding utf8 -Force
+
+    $psArgs = ""
+    if ($DEBUG) {
+        $psArgs += " -NoExit -WindowStyle Normal"
+    } else {
+        $psArgs += " -WindowStyle Hidden"
+    }
+    $psArgs += " -ExecutionPolicy Bypass $ExtraPSArguments"
+    $psArgs = "$psArgs -File `"$genScriptPath`""
+
+    # schedule a shutdown to prevent Packer from exiting..
+    shutdown /r /f /t 600 /c "Will reboot when VM script decides from now on! Please stand by..."
+
+    $schedTask = @{
+        Action = (New-ScheduledTaskAction -Execute 'powershell.exe' -Argument $psArgs)
+        Trigger = (Get-CimClass "MSFT_TaskRegistrationTrigger" `
+            -Namespace "Root/Microsoft/Windows/TaskScheduler")
+        Settings = (New-ScheduledTaskSettingsSet -AllowStartIfOnBatteries `
+            -DontStopIfGoingOnBatteries -StartWhenAvailable -DontStopOnIdleEnd)
+        TaskName = $schedTaskName
+    }
+    Register-ScheduledTask @schedTask | Write-Verbose
+    Write-Verbose "Scheduled powershell.exe with args: '$psArgs'"
+
+}
+
+function Restart-VMWinRM {
+    Set-Service WinRM -StartupType Automatic
+    Restart-Service winrm | out-null
+}
+
 Export-ModuleMember -Function Invoke-VMScriptTask
+Export-ModuleMember -Function Invoke-VMRebootingTask
+Export-ModuleMember -Function Restart-VMWinRM
 
