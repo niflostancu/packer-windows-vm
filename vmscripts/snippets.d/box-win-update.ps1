da23ec8ddd104e98eb11f2acbaa5250c7357db9d
Stancu Florin <niflostancu@gmail.com>
2026-01-27 12:18:51 +0200

    winupdate: migrate to VMRebootingTask

--- vmscripts/snippets.d/box-win-update.ps1
+++ vmscripts/snippets.d/box-win-update.ps1
@@ -1,2 +1,17 @@
+# BoxStarter package to install windows updates
+
+. "C:\Windows\vmscripts\common.ps1"
+
+Write-Host "winupd: processing WindowsUpdate..."
 Install-WindowsUpdate -AcceptEula
+
+if (Test-PendingReboot) {
+	Write-Host "winupd: Reboot requested..."
+	Invoke-Reboot
+	exit 0
+}
+
+Write-Host "winupd: Finished!"
+Write-Host "winupd: Re-enabling WinRM..."
+Restart-VMWinRM
 
--- vmscripts/snippets.d/update-windows-boxstarter.ps1
+++ vmscripts/snippets.d/update-windows-boxstarter.ps1
@@ -1,5 +1,10 @@
 # Brings Windows up to date.
 
+if ( $env:VM_NO_UPGRADE -eq '1' ) {
+    Write-Output "Upgrade skipped!"
+    return
+}
+
 # Set DeliveryOptimization to Internet
 $regKey = "HKLM:\SOFTWARE\Policies\Microsoft\Windows\DeliveryOptimization" 
 New-Item -Path $regKey -Force | out-null
@@ -8,5 +13,7 @@ Set-ItemProperty -Path $RegKey -Name DODownloadMode -Type Dword -Value 3 | out-n
 # Start the updates
 Write-Output 'Updating Windows, this may take some time...'
 
-Install-BoxstarterPackage -PackageName "$VMSCRIPTS\snippets.d\box-win-update.ps1"
+# $global:DEBUG = $true
+Invoke-VMRebootingTask -TaskID "box_winupdate" `
+    -ScriptSnippet "Install-BoxstarterPackage -PackageName `"$VMSCRIPTS\snippets.d\box-win-update.ps1`""
 
